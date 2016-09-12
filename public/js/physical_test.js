fb4c6cfbe318d73284d05cc955fcded227c64161
Carlos Graça <carlosgraca1713@gmail.com>
2016-09-12 09:52:33 -0100

    New Form

--- public/js/physical_test.js
+++ public/js/physical_test.js
@@ -10,16 +10,64 @@ $('document').ready(function(){
   //  $('#dt_test').val(date);
 
 
-    $('#gerar_idade').on('click',function(){
+    $('#dt_nasc').on('change',function(){
     	var dt_test = new Date($('#dt_test').val());
     	var dt_nasc = new Date($('#dt_nasc').val());
-        gerar_idade(dt_test,dt_nasc);
+        var idade = get_idade(dt_test,dt_nasc);
         console.log($('#dt_nasc').val());
+        $('#age').val(idade);
+    });
+
+    $('#peso').on('change',function(){
+        var peso = $('#peso').val();
+        if (peso) {
+            var estatura = $('#estatura').val();
+            var imc = get_imc(peso,estatura);
+            $('#imc').val(imc);
+            var idade = $('#age').val();
+            if (((idade >= 15) || (idade <= 19)) &&  ((imc >= 18) || (imc <= 21.6)) ){                
+                $('#class').val('Exelente');
+            }else{
+                $('#class').val(' ');
+            } 
+        }  
+    });
+
+    $('#estatura').on('change',function(){
+        var estatura = $('#estatura').val();
+        if (estatura) {
+            var peso = $('#peso').val();
+            var imc = get_imc(peso,estatura);
+            var idade = $('#age').val();
+            $('#imc').val(imc);
+            if (((idade >= 15) || (idade <= 19)) &&  ((imc >= 18) || (imc <= 21.6)) ){                
+                $('#class').val('Exelente');
+            }else{
+                $('#class').val(' ');
+            }            
+        }  
+    });
+
+    $('#gordura').on('change',function(){
+        var g = $('#gordura').val();
+        var p = $('#peso').val();
+        var pg = get_peso_gordura(g,p);
+        $('#peso_gordura').val(pg);
+
+        var mm = get_massa_magra(p,pg);
+        $('#massa_magra').val(mm);
+    });
+
+    $('#peso_gordura').on('change',function(){
+        var pg = $('#peso_gordura').val();
+        var p = $('#peso').val();
+        var mm = get_massa_magra(p,pg);
+        $('#massa_magra').val(mm);
     });
 
 });
 
-function gerar_idade(date_test,date_nasc){
+function get_idade(date_test,date_nasc){
 	var idade;
     var dt_test = new Date(date_test);
     var dt_nasc = new Date(date_nasc);
@@ -27,9 +75,20 @@ function gerar_idade(date_test,date_nasc){
     test = dt_test.getDate()+(dt_test.getMonth()*30)+(dt_test.getFullYear()*365);
     var nasc = dt_nasc.getDate()+(dt_nasc.getMonth()*30)+(dt_nasc.getFullYear()*365);
     idade = (test - nasc)/365;
-    console.log(idade);
+    return idade;    
+}
 
-    $('#age').val(idade);
+function get_imc(peso,estatura){
+    var imc = peso/(estatura/100*estatura/100);
+    return imc;
+}
 
+function get_peso_gordura(gordura,peso){
+    var pg = gordura/100*peso;
+    return pg;
+}
 
+function get_massa_magra(peso,pesso_gordura){
+    var mm = peso-pesso_gordura;
+    return mm;
 }
--- resources/views/tests/form.blade.php
+++ resources/views/tests/form.blade.php
@@ -1,4 +1,21 @@
+<style type="text/css">
+	.h-divider{
+		 margin-top:5px;
+		 margin-bottom:5px;
+		 height:1px;
+		 border-top:3px solid #D2D6DE;
+		 size: 30;
+		 margin-left: 15px;
+		 margin-right: 15px;		
+    }
+    .title{
+    	padding-top:15x;
+    	margin-left: 15px;
+    }
+</style>
 <div class="row">
+    <span ><strong class="title">Dados Pessoais</strong></span>
+    <hr class="h-divider" >
     <div class="col-lg-3 col-md-4 col-sm-6">
 		<div class="form-group form-group-sm">
 			{!! Form::label('name','Name:') !!}
@@ -49,7 +66,95 @@
       {!! Form::select('sexo', [0=>'Escolha a Opção',1=>'Masculino',2=>'Feminino'],NULL, ['class'=>'form-control']) !!}
     </div>
   </div>
-  <div class="col-lg-3 col-md-4 col-sm-6">
-	   <button type="button" id="gerar_idade" class="btn btn-primary" style="margin-top:20px;">Gerar Idade</button>
-  </div>
 </div>
+
+<div class="row">
+    <span ><strong class="title">Indice Massa Corporal</strong></span>
+    <hr class="h-divider" >	
+    <div class="col-lg-3 col-md-4 col-sm-6">
+		<div class="form-group form-group-sm">
+			{!! Form::label('estatura','Estatura (cm):') !!}
+			{!! Form::text('estatura', null, ['class'=>'form-control']) !!}
+		</div>
+	</div>
+    <div class="col-lg-3 col-md-4 col-sm-6">
+		<div class="form-group form-group-sm">
+			{!! Form::label('peso','Peso (kg):') !!}
+			{!! Form::text('peso', null, ['class'=>'form-control']) !!}
+		</div>
+	</div>
+	<div class="col-lg-3 col-md-4 col-sm-6">
+		<div class="form-group form-group-sm">
+			{!! Form::label('imc','IMC  kg/m²:') !!}
+			{!! Form::text('imc', null, ['class'=>'form-control']) !!}
+		</div>
+	</div>
+	<div class="col-lg-3 col-md-4 col-sm-6">
+		<div class="form-group form-group-sm">
+			{!! Form::label('class','Class.:') !!}
+			{!! Form::text('class', null, ['class'=>'form-control']) !!}
+		</div>
+	</div>
+</div>
+<div class="row">
+	<span ><strong class="title">Fracionamento da Composição Corporal</strong></span>
+    <hr class="h-divider" >	
+    <div class="col-lg-3 col-md-4 col-sm-6">
+		<div class="form-group form-group-sm">
+			{!! Form::label('gordura','% de Gordura:') !!}
+			{!! Form::text('gordura', null, ['class'=>'form-control']) !!}
+		</div>
+	</div>
+	<div class="col-lg-3 col-md-4 col-sm-6">
+		<div class="form-group form-group-sm">
+			{!! Form::label('peso_gordura','Peso da gordura (kg):') !!}
+			{!! Form::text('peso_gordura', null, ['class'=>'form-control']) !!}
+		</div>
+	</div>
+	<div class="col-lg-3 col-md-4 col-sm-6">
+		<div class="form-group form-group-sm">
+			{!! Form::label('massa_magra','Massa magra (kg)') !!}
+			{!! Form::text('massa_magra', null, ['class'=>'form-control']) !!}
+		</div>
+	</div>
+</div>
+<div class="row">
+	<span ><strong class="title">Medidas Circunferências</strong></span>
+    <hr class="h-divider" >
+    <div class="col-lg-3 col-md-4 col-sm-6">
+		<div class="form-group form-group-sm">
+			{!! Form::label('circunferencia','Circunferência: Busto (cm):') !!}
+			{!! Form::text('circunferencia', null, ['class'=>'form-control']) !!}
+		</div>
+	</div>
+	<div class="col-lg-3 col-md-4 col-sm-6">
+		<div class="form-group form-group-sm">
+			{!! Form::label('cintura','Cintura  (cm):') !!}
+			{!! Form::text('cintura', null, ['class'=>'form-control']) !!}
+		</div>
+	</div>
+	<div class="col-lg-3 col-md-4 col-sm-6">
+		<div class="form-group form-group-sm">
+			{!! Form::label('quadril','Quadril  (cm)') !!}
+			{!! Form::text('quadril', null, ['class'=>'form-control']) !!}
+		</div>
+	</div>
+	<div class="col-lg-3 col-md-4 col-sm-6">
+		<div class="form-group form-group-sm">
+			{!! Form::label('coxa','Coxa (cm):') !!}
+			{!! Form::text('coxa', null, ['class'=>'form-control']) !!}
+		</div>
+	</div>
+	<div class="col-lg-3 col-md-4 col-sm-6">
+		<div class="form-group form-group-sm">
+			{!! Form::label('panturrilha','C. Panturrilha (cm):') !!}
+			{!! Form::text('panturrilha', null, ['class'=>'form-control']) !!}
+		</div>
+	</div>
+	<div class="col-lg-3 col-md-4 col-sm-6">
+		<div class="form-group form-group-sm">
+			{!! Form::label('rel_cin_qua','Relação Cintura/Quadril') !!}
+			{!! Form::text('rel_cin_qua', null, ['class'=>'form-control']) !!}
+		</div>
+	</div>
+ </div>

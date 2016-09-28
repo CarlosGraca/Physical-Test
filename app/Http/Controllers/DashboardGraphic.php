ec6a116ddc18c3c90699b0e809cefacdcb9a7ec5
Flameu Fortes <af131588@us.edu.cv>
2016-09-28 12:57:22 -0100

    updates

--- app/Http/Controllers/DashboardGraphic.php
+++ app/Http/Controllers/DashboardGraphic.php
@@ -28,14 +28,10 @@ class DashboardGraphic extends Controller
       $sheets = Sheet::select(\DB::raw('MONTHNAME(created_at) as month'), \DB::raw("DATE_FORMAT(created_at,'%Y-%m') as monthNum"), DB::raw('count(*) as sheet_count'))
       ->whereBetween('created_at', [$startDate, $endDate])->groupBy(DB::raw("MONTHNAME(created_at)"))->groupBy(DB::raw("DATE_FORMAT(created_at,'%Y-%m')"))->get();
 
-      $dados = Test::select(\DB::raw('MONTHNAME(tests.created_at) as month'), \DB::raw("DATE_FORMAT(tests.created_at,'%Y-%m') as monthNum"), DB::raw('count(*) as test_count'))
-      ->whereBetween('tests.created_at', [$startDate, $endDate])->groupBy(DB::raw("MONTHNAME(tests.created_at)"))->groupBy(DB::raw("DATE_FORMAT(tests.created_at,'%Y-%m')"))
-       ->join('sheets',DB::raw("DATE_FORMAT(tests.created_at,'%Y-%m')"), '=', DB::raw("DATE_FORMAT(sheets.created_at,'%Y-%m')"))->get();
-
     //  $tests = DB::select("select MONTHNAME(dt_test) as month, DATE_FORMAT(dt_test,'%Y-%m') as monthNum, count(tests.id) as test_count from `tests` group by DATE_FORMAT(dt_test,'%Y-%m'), MONTHNAME(dt_test)");
     //  $sheets = DB::select("select MONTHNAME(created_at) as month, DATE_FORMAT(created_at,'%Y-%m') as monthNum, count(sheets.id) as sheet_count from `sheets` group by DATE_FORMAT(created_at,'%Y-%m'), MONTHNAME(created_at)");
 
-      $data = ['tests'=>$tests,'sheets'=>$sheets,'dados'=>$dados];
+      $data = ['tests'=>$tests,'sheets'=>$sheets];
 
       return $data;
     }

a9d63fc2306fd25ce7ef98218696418344b746fe
Flameu Fortes <af131588@us.edu.cv>
2016-09-29 10:39:08 -0100

    Dashboard Test
    Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -52,30 +52,7 @@
 	    </div>
 		</div>
 		<div class="col-md-6 col-sm-12 col-xs-12">
-				<!-- BAR CHART -->
-				<div class="box box-success">
-					<div class="box-header with-border">
-						<i class="fa fa-bar-chart"></i>
-						<h3 class="box-title">{{ trans('adminlte_lang::message.chart_bar_title') }}</h3>
-
-						<div class="box-tools">
-							<button type="button" class="btn btn-primary btn-sm daterange pull-right" data-toggle="tooltip" title="Date range">
-								<i class="fa fa-calendar"></i></button>
-								<button type="button" class="btn btn-primary btn-sm pull-right" data-widget="collapse" data-toggle="tooltip" title="Collapse" style="margin-right: 5px;">
-                  <i class="fa fa-minus"></i></button>
-						</div>
-					</div>
-					<div class="box-body">
-						<div class="chart">
-							<canvas id="barChart" style="height:230px"></canvas>
-						</div>
-					</div>
-					<div class="box-footer">
-						<span class="range-date"></span>
-					</div>
-					<!-- /.box-body -->
-				</div>
-				<!-- /.box -->
+				@include('dashboard.bar_chart',[])
 		</div>
 	</div>
 @endsection
